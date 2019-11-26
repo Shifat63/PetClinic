6840663c3349be5150703fd83c745761cfbeaac4
MD. Emtazul Haque <shifat63csedu@gmail.com>
2019-11-27 05:53:14 +0600

    Project initialized

---
+++ src/test/java/shifat63/petclinic/shifat63petclinic/Shifat63PetclinicApplicationTests.java
@@ -0,0 +1,13 @@
+package shifat63.petclinic.shifat63petclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Shifat63PetclinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
