7915378d7fa5fefe7888d4d6ab168bf183137974
MD. Emtazul Haque <shifat63csedu@gmail.com>
2019-11-28 23:41:31 +0600

    Index pages and controllers for vets and owners created. Closes #12 and #13

---
+++ PetClinicWeb/src/main/java/PetClinic/Controllers/OwnerController.java
@@ -0,0 +1,13 @@
+package PetClinic.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class OwnerController {
+
+    @RequestMapping({"/owners","/owners/index","/owners/index.html"})
+    public String index(){
+        return "owners/index";
+    }
+}
