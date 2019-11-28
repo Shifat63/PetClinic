24f1e27966dbf20c0a2fc6bc448776b263c1493f
MD. Emtazul Haque <shifat63csedu@gmail.com>
2019-11-28 16:09:54 +0600

    Base Entity created. Closes #9

--- PetClinicData/src/main/java/Model/Pet.java
+++ PetClinicData/src/main/java/Model/Pet.java
@@ -2,7 +2,7 @@ package Model;
 
 import java.time.LocalDate;
 
-public class Pet {
+public class Pet extends BaseEntity {
     private Owner owner;
     private PetType petType;
     private LocalDate birthDate;
--- PetClinicData/src/main/java/Model/PetType.java
+++ PetClinicData/src/main/java/Model/PetType.java
@@ -1,6 +1,6 @@
 package Model;
 
-public class PetType {
+public class PetType extends BaseEntity {
     private String name;
 
     public String getName() {
