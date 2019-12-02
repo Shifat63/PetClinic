b3d90a117da969b34e18374df9fbca31eabca3c5
MD. Emtazul Haque <shifat63csedu@gmail.com>
2019-12-02 23:31:09 +0600

    Closes issue #35.

--- PetClinicData/src/main/java/PetClinicData/Model/Vet.java
+++ PetClinicData/src/main/java/PetClinicData/Model/Vet.java
@@ -1,8 +1,13 @@
 package PetClinicData.Model;
 
+import javax.persistence.*;
 import java.util.Set;
 
+@Entity
+@Table(name = "vet")
 public class Vet extends Person {
+    @ManyToMany
+    @JoinTable(name = "vet_speciality", joinColumns = @JoinColumn(name = "vet_id"), inverseJoinColumns = @JoinColumn(name = "speciality_id"))
     private Set<Speciality> specialities;
 
     public Set<Speciality> getSpecialities() {
