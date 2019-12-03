6509cfc98f071eb3e68b77b40ce0d3c3aa594205
MD. Emtazul Haque <shifat63csedu@gmail.com>
2019-12-04 01:20:04 +0600

    Closes issue #40.

--- PetClinicWeb/src/main/java/PetClinic/Bootstrap/DataLoader.java
+++ PetClinicWeb/src/main/java/PetClinic/Bootstrap/DataLoader.java
@@ -17,13 +17,15 @@ public class DataLoader implements CommandLineRunner {
     private final VetService vetService;
     private final PetTypeService petTypeService;
     private final SpecialityService specialityService;
+    private final VisitService visitService;
 
-    public DataLoader(OwnerService ownerService, PetService petService, VetService vetService, PetTypeService petTypeService, SpecialityService specialityService) {
+    public DataLoader(OwnerService ownerService, PetService petService, VetService vetService, PetTypeService petTypeService, SpecialityService specialityService, VisitService visitService) {
         this.ownerService = ownerService;
         this.petService = petService;
         this.vetService = vetService;
         this.petTypeService = petTypeService;
         this.specialityService = specialityService;
+        this.visitService = visitService;
     }
 
     @Override
@@ -84,9 +86,40 @@ public class DataLoader implements CommandLineRunner {
         emtazul.setCity("Bonn");
 
         tommy.setOwner(emtazul);
+
+        Visit tommyVisit1 = new Visit();
+        tommyVisit1.setDate(LocalDate.now());
+        tommyVisit1.setDescription("Tommy visit 1");
+        tommyVisit1.setPet(tommy);
+        visitService.save(tommyVisit1);
+
+        Visit tommyVisit2 = new Visit();
+        tommyVisit2.setDate(LocalDate.now());
+        tommyVisit2.setDescription("Tommy visit 2");
+        tommyVisit2.setPet(tommy);
+        visitService.save(tommyVisit2);
+
+        Set<Visit> tommyVisits = new HashSet<>();
+        tommyVisits.add(tommyVisit1);
+        tommyVisits.add(tommyVisit2);
+
+        tommy.setVisits(tommyVisits);
         petService.save(tommy);
+
         vulo.setOwner(emtazul);
+
+        Visit vuloVisit1 = new Visit();
+        vuloVisit1.setDate(LocalDate.now());
+        vuloVisit1.setDescription("Vulo visit 1");
+        vuloVisit1.setPet(vulo);
+        visitService.save(vuloVisit1);
+
+        Set<Visit> vuloVisits = new HashSet<>();
+        vuloVisits.add(vuloVisit1);
+
+        vulo.setVisits(vuloVisits);
         petService.save(vulo);
+
         Set<Pet> emtazulPets = new HashSet<>();
         emtazulPets.add(tommy);
         emtazulPets.add(vulo);
@@ -102,7 +135,26 @@ public class DataLoader implements CommandLineRunner {
         torikul.setTelephone("34567");
 
         hosko.setOwner(torikul);
+
+        Visit hoskoVisit1 = new Visit();
+        hoskoVisit1.setDate(LocalDate.now());
+        hoskoVisit1.setDescription("Hosko Visit 1");
+        hoskoVisit1.setPet(hosko);
+        visitService.save(hoskoVisit1);
+
+        Visit hoskoVisit2 = new Visit();
+        hoskoVisit2.setDate(LocalDate.now());
+        hoskoVisit2.setDescription("Hosko Visit 2");
+        hoskoVisit2.setPet(hosko);
+        visitService.save(hoskoVisit2);
+
+        Set<Visit> hoskoVisits = new HashSet<>();
+        hoskoVisits.add(hoskoVisit1);
+        hoskoVisits.add(hoskoVisit2);
+
+        hosko.setVisits(hoskoVisits);
         petService.save(hosko);
+
         Set<Pet> torikulPets = new HashSet<>();
         torikulPets.add(hosko);
         torikul.setPets(torikulPets);
@@ -117,14 +169,26 @@ public class DataLoader implements CommandLineRunner {
         ibrahim.setTelephone("98765");
 
         fire.setOwner(ibrahim);
+
+        Visit fireVisit1 = new Visit();
+        fireVisit1.setDate(LocalDate.now());
+        fireVisit1.setDescription("Fire Visit 1");
+        fireVisit1.setPet(fire);
+        visitService.save(fireVisit1);
+
+        Set<Visit> fireVisits = new HashSet<>();
+        fireVisits.add(fireVisit1);
+
+        fire.setVisits(fireVisits);
         petService.save(fire);
+
         Set<Pet> ibrahimPets = new HashSet<>();
         ibrahimPets.add(fire);
         ibrahim.setPets(ibrahimPets);
 
         ownerService.save(ibrahim);
 
-        System.out.println("Owner and Pet data loaded");
+        System.out.println("Owner, Pet and Visit data loaded");
 
         Vet tamim = new Vet();
         tamim.setFirstName("Tamim");
